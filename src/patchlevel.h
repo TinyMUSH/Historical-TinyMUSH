a6b5ef7d69d1f5e09fdd867576a56d4bb35666a6
dpassmor <>
1997-06-21 08:24:30 +0000

    *** empty log message ***

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -4,8 +4,8 @@
 #include "copyright.h"
 
 #define	MUX_VERSION		"1.4"		/* Base version number */
-#define PATCHLEVEL		4		/* Patch sequence number */
-#define	MUX_RELEASE_DATE	"4/15/97"	/* Source release date */
+#define PATCHLEVEL		5		/* Patch sequence number */
+#define	MUX_RELEASE_DATE	"6/21/97"	/* Source release date */
 
 /*#define BETA			1*/		/* Define if a BETA release */
 
