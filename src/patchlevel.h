fae79435b3182872097cca6633a564ed25ad18e1
cvs <>
1998-12-31 16:10:19 +0000

    beta 2 patchlevel

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -3,9 +3,9 @@
 
 #include "copyright.h"
 
-#define	MUSH_VERSION		"3.0"		/* Base version number */
+#define	MUSH_VERSION		"3.0b2"		/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"7/28/97"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"12/31/98"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
 
