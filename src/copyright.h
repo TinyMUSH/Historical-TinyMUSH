5b6cf1f32dd0cf31e4fc3d9f67b0e3f076192e4f
cvs <>
1999-05-22 19:59:31 +0000

    Updated to new TinyMUD/TinyMUSH 2.0 copyright. Mucked with the credits and so forth. David needs to look at it, make his own changes, and OK it.

--- src/copyright.h
+++ src/copyright.h
@@ -2,78 +2,121 @@
 /* -*-C-*- */
 /* $Id$ */
 
+#ifndef _copyright_h
+#define _copyright_h
+
 /*
- * TinyMUX source code is maintained by David Passmore
- * Comments should be sent to lauren@sneakers.org.
- * TinyMUX is Copyright (c) 1995 by David Passmore.
+ * TinyMUSH 3.0 Source Code
+ * Copyright (c) 1999 by Lydia Leong and David Passmore.
+ * All rights reserved.
+ * See http://www.godlike.com/3.0/ for more information.
  *
- * Many parts of the source code, and the help text were derived from
- * TinyMUSH 2.2:
- * TinyMUSH 2.2 Source was written in part by
- *     Jean Marie Diaz, Lydia Leong, and Devin Hooker
- *     2.2 initial alpha release 9/21/94.
- *         final beta snapshot (hopefully) 3/4/95
+ * TinyMUX Source Code
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999, David Passmore.
  *
- * Many parts of the source code, and the help text were derived from
- * PennMUSH 1.50:
+ * TinyMUSH 2.2 Source Code
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999, Lydia Leong, Jean Marie Diaz,
+ * and Deborah Wilson-HOoker.
  *
- * Patchlevel 12 is based on PennMUSH pl10, the last version of PennMUSH
- * released by Amberyl. All copyright notices above continue to apply.
- * Versions of PennMUSH prior to pl10 are written in part by
- *      Alan Schwartz
- * Thanks and praise are due to:
- * Ralph Melton (Rhyanna@Castle D'Image) for bugs reports, patches, ideas,
- *    and extensions
- * T. Alexander Popiel for bug reports, patches, ideas, and extensions, too.
- * Al Brown (Kalkin@DarkZone) for many clever ideas
- * The many other people who've sent in bug reports
- * The admin and players of Dune II, who put up with a lot of broken code
- * Special thanks to Amberyl for all her help.
- *      - Paul/Javelin (Alan Schwartz, alansz@mellers1.psych.berkeley.edu)
- *
- * Based on TinyMUSH 2.0 Source code
- * Copyright (c) 1991 Joseph Traub and Glenn Crocker
+ * TinyMUSH 2.0 Source code
+ * Copyright (c) 1991, Joseph Traub and Glenn Crocker. All rights reserved.
  *
  * Based on TinyMUD code
- * Copyright (c) 1989, 1990 by David Applegate, James Aspnes, Timothy Freeman,
- *                             and Bennet Yee.
+ * Copyright (c) 1995, David Applegate, James Aspnes, Timothy Freeman,
+ *                     and Bennet Yee. All rights reserved.
  *
- * This material was developed by the above-mentioned authors.  Permission to
- * copy this software, to redistribute it, and to use it for any purpose is
- * granted, subject to the following restrictions and understandings.
+ * (Modified by these authors from the original 1989, 1990 TinyMUD copyright.)
  *
- * 1. Any copy made of this software must include this copyright notice in
- *    full.
+ * ---
  *
- * 2. Users of this software agree to make their best efforts
- *    (a) to return to the above-mentioned authors any improvements or
- *        extensions that they make, so that these may be included in future
- *        releases; and
- *    (b) to inform the authors of noteworthy uses of this software.
+ * TinyMUSH 3.0 is a derivation of TinyMUSH 2.2 and TinyMUX, both of which
+ * are derivations of TinyMUSH 2.0, which in turn is based upon TinyMUD.
+ * There are also portions of source code derived from PennMUSH 1.50.
  *
- * 3. All materials developed as a consequence of the use of this software
- *    shall duly acknowledge such use, in accordance with the usual standards
- *    of acknowledging credit in academic research.
+ * This copyright notice is based upon the latest TinyMUD and TinyMUSH 2.0
+ * copyright, with additional specific copyright information for TinyMUSH 3.0.
  *
- * 4. The authors have made no warrantee or representation that the operation
- *    of this software will be error-free, and the authors are under no
- *    obligation to provide any services, by way of maintenance, update, or
- *    otherwise.
+ * ---
  *
- * 5. In conjunction with products arising from the use of this material,
- *    there shall be no use of the names of the authors, of Carnegie-Mellon
- *    University, nor of any adaptation thereof in any advertising,
- *    promotional, or sales literature without prior written consent from
- *    the authors and both Carnegie-Mellon University Case Wester Reserve
- *    University in each case.
+ * Revised TinyMUD / TinyMUSH 2.0 copyright:
  *
- * Credits:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source code distributions
+ * retain the above copyright notice and this paragraph in its entirety,
+ * and (2) distributions including binary code include the above copyright
+ * notice and this paragraph in its entirety in the documentation or other
+ * materials provided with the distribution.  The names of the copyright
+ * holders may not be used to endorse or promote products derived from 
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ---
+ *
+ * TinyMUSH 3.0 Copyright
+ *
+ * Users of this software incur the obligation to make their best efforts
+ * to inform the authors of noteworthy uses of this software.
+ *
+ * All materials developed as a consequence of the use of this software
+ * shall duly acknowledge such use, in accordance with the usual standards
+ * of acknowledging credit in academic research.
+ *
+ * TinyMUSH 3.0 may be used for commercial, for-profit applications, subject
+ * to the following conditions: You must acknowledge the origin of the 
+ * software, retaining this copyright notice in some prominent place.
+ * You may charge only for access to the service you provide, not for
+ * the TinyMUSH 3.0 software itself. You must inform the authors of
+ * any commercial use of this software.
+ *
+ * The authors have made no warranty or representation that the operation
+ * of this software will be error-free. The authors are under no obligation
+ * to provide any services, by way of maintenance, update, or otherwise.
+ *
+ * ---
+ *
+ * TinyMUSH 2.0 Credits:
  *    Lawrence Foard:
  *      Wrote the original TinyMUSH 1.0 code from which this later derived.
  *    Jin (and MicroMUSH):
  *      Made many, many changes to the code that improved it immensely.
+ *    Robert Hood (virus):
+ *      Modified the interface.c code to support alots of users.
  *    Lachesis:
  *      Introduced the idea of property lists to TinyMUCK
  *    Many others:
  *      Many features borrowed from other muds.
+ *
+ * ---
+ *
+ * TinyMUSH 2.2 Credits:
+ *
+ * TinyMUSH 2.2 is the result of the efforts of many, many people. A few
+ * deserve special mention here: Andrew Molitor, Russ Smith, Mark Vasoll,
+ * and Dave McCracken for much in the way of code and advice; Alan Schwartz
+ * and T. Alexander "Talek" Popiel (PennMUSH) and David Passmore (TinyMUX)
+ * for ideas and code; and John Batzel (and the rest of the PernMUSH
+ * wizcrew) for beta-testing, many ideas, and a lot of patience.
+ *
+ * ---
+ *
+ * TinyMUSH 3.0 Credits:
+ *
+ * Many people contributed to the end result which is TinyMUSH 3.0. They
+ * include:
+ *
+ *	- Fred "Iago" Hicks, for the logo
+ *	- Deborah Wilson-Hooker, T. Alexander Popiel, JT Traub, and
+ *	  Andrew Molitor, as always
+ *	- The players of From the Ashes, Babylon 5: The Last Best Hope,
+ *	  and Blood of Amber, for alpha testing
+ *	- The TinyMUX and Hogs lists, and the players of the Godlike Edge,
+ *	  for everything
+ *
+ * ... and everyone who participated in the religious wars between TinyMUSH
+ *     and TinyMUX, which ultimately spurred our decision to merge them. :)
  */
+
+#endif /* _copyright_h */
