641ddd17da1fa3b7c35df3898a4eb697ae5c4e5e
cvs <>
1999-08-31 04:58:28 +0000

    MUSH3_alpha11

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -3,9 +3,9 @@
 
 #include "copyright.h"
 
-#define	MUSH_VERSION		"3.0a10"	/* Base version number */
+#define	MUSH_VERSION		"3.0a11"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"07/20/99"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"08/30/99"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
 
