9cb10ec38f364abc019f10b150a207fc462ceb9d
cvs <>
1999-10-09 22:56:50 +0000

    Don't redo ANSI hiliting for editing if ansi_colors is not enabled.

--- src/set.c
+++ src/set.c
@@ -1382,9 +1382,14 @@ char *src, **dst, **returnstr, *from, *to;
     edit_string(src, dst, from, to);
 
     *returnstr = alloc_lbuf("edit_string_ansi");
-    edit_string(src, returnstr, from,
-		tprintf("%s%s%s%s", ANSI_HILITE, to, ANSI_NORMAL,
-			ANSI_NORMAL));
+
+    if (mudconf.ansi_colors) {
+	edit_string(src, returnstr, from,
+		    tprintf("%s%s%s%s", ANSI_HILITE, to, ANSI_NORMAL,
+			    ANSI_NORMAL));
+    } else {
+	StringCopy(*returnstr, *dst);
+    }
 }
 
 void do_edit(player, cause, key, it, args, nargs)
