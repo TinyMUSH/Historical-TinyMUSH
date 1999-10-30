2d319f421e7e5d33b39a38c33d28c18025525408
cvs <>
1999-10-30 22:17:17 +0000

    3.0b15 patchlevel 1 -- no cvs tagging, just a patch release.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -4,7 +4,7 @@
 #include "copyright.h"
 
 #define	MUSH_VERSION		"3.0b15"	/* Base version number */
-#define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"10/25/99"	/* Source release date */
+#define PATCHLEVEL		1		/* Patch sequence number */
+#define	MUSH_RELEASE_DATE	"10/30/99"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
