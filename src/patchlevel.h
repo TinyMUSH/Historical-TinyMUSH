e65a8e2649b1ee55b63d149f36618f59056a98de
dpassmor <>
2000-04-22 08:09:41 +0000

    MUSH3_stable2

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -3,8 +3,8 @@
 
 #include "copyright.h"
 
-#define	MUSH_VERSION		"3.0b20"	/* Base version number */
+#define	MUSH_VERSION		"3.0s2"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"12/29/99"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"4/22/2000"	/* Source release date */
 
-#define BETA			1		/* Define if a BETA release */
+/* #define BETA			1	*/	/* Define if a BETA release */
