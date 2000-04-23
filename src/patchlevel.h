7a0d315efbf6986ec8e89f076a42e771c35d3635
cvs <>
2000-04-23 22:45:12 +0000

    The 'real' 3.0b20, hopefully.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -3,8 +3,8 @@
 
 #include "copyright.h"
 
-#define	MUSH_VERSION		"3.0a1"	/* Base version number */
+#define	MUSH_VERSION		"3.0b20"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"4/22/2000"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"4/23/2000"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
