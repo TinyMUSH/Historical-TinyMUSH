0445e413cbf8da78a6e8c46277742a1e57316d7b
lwl <>
2000-05-12 22:44:55 +0000

    Updated release date.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -5,6 +5,6 @@
 
 #define	MUSH_VERSION		"3.0b20"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"5/8/2000"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"5/13/2000"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
