ea4db7fd690a7c8a41fb6b21ada9d189aecb4888
lwl <>
2000-06-28 18:39:27 +0000

    Reflect alpha status rather than beta status of 3.1, update date. (Tagging snapshot.)

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -6,9 +6,9 @@
 #ifndef __PATCHLEVEL_H
 #define __PATCHLEVEL_H
 
-#define	MUSH_VERSION		"3.1b1"	/* Base version number */
+#define	MUSH_VERSION		"3.1a2"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"4/23/2000"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"6/28/2000"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
 
