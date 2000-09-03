5725404e8a78c2c725c40571f71379c23f967076
dpassmor <>
2000-09-03 20:27:59 +0000

    Updated to MUSH31_alpha3

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -6,9 +6,9 @@
 #ifndef __PATCHLEVEL_H
 #define __PATCHLEVEL_H
 
-#define	MUSH_VERSION		"3.1a2"	/* Base version number */
+#define	MUSH_VERSION		"3.1a3"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"6/28/2000"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"9/3/2000"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
 
