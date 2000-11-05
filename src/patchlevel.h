f477371d9ea59f977a419cf840fbe3e152aa50bf
dpassmor <>
2000-11-05 23:39:21 +0000

    Told you I'd screw it up. Forgot to update patchlevel.h.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -3,8 +3,8 @@
 
 #include "copyright.h"
 
-#define	MUSH_VERSION		"3.0b21"	/* Base version number */
+#define	MUSH_VERSION		"3.0b22"	/* Base version number */
 #define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"6/12/2000"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"11/04/2000"	/* Source release date */
 
 #define BETA			1		/* Define if a BETA release */
