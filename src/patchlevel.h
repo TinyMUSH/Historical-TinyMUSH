f85471f35c3406a0fc82d1f807d3135f99413023
lwl <>
2000-12-05 20:48:01 +0000

    3.0p1 - fix xvars_clr() crash bug.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -4,7 +4,7 @@
 #include "copyright.h"
 
 #define	MUSH_VERSION		"3.0"		/* Base version number */
-#define PATCHLEVEL		0		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"12/01/2000"	/* Source release date */
+#define PATCHLEVEL		1		/* Patch sequence number */
+#define	MUSH_RELEASE_DATE	"12/05/2000"	/* Source release date */
 
 /* #define BETA			1	*/	/* Define if a BETA release */
