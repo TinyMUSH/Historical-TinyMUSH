d9f9e96451475de0e3beaeed616d95b71e1ab682
dpassmor <>
2001-01-21 06:29:18 +0000

    Extended TEST_MALLOC functionality to include byte counts

--- src/config.h
+++ src/config.h
@@ -114,15 +114,15 @@
 
 #ifdef TEST_MALLOC
 extern int malloc_count;
-#define XMALLOC(x,y) (fprintf(stderr,"Malloc: %s\n", (y)), malloc_count++, \
+#define XMALLOC(x,y) (fprintf(stderr,"Malloc: %s/%d\n", (y), (x)), malloc_count++, \
                     (char *)malloc((x)))
-#define XCALLOC(x,z,y) (fprintf(stderr,"Calloc: %s\n", (y)), malloc_count++, \
+#define XCALLOC(x,z,y) (fprintf(stderr,"Calloc: %s/%d\n", (y), (x*z)), malloc_count++, \
                     (char *)calloc((x),(z)))
-#define XREALLOC(x,z,y) (fprintf(stderr,"Realloc: %s\n", (y)), \
+#define XREALLOC(x,z,y) (fprintf(stderr,"Realloc: %s/%d\n", (y), (z)), \
                     (char *)realloc((x),(z)))
-#define XSTRDUP(x,y) (fprintf(stderr,"Strdup: %s\n", (y)), malloc_count++, \
+#define XSTRDUP(x,y) (fprintf(stderr,"Strdup: %s/%d\n", (y), (strlen(x))), malloc_count++, \
                     (char *)strdup((x)))
-#define XSTRNDUP(x,z,y) (fprintf(stderr,"Strndup: %s\n", (y)),malloc_count++, \
+#define XSTRNDUP(x,z,y) (fprintf(stderr,"Strndup: %s/%d\n", (y), (z)),malloc_count++, \
                     (char *)strndup((x),(z)))
 #define XFREE(x,y) (fprintf(stderr, "Free: %s\n", (y)), \
                     ((x) ? malloc_count--, free((void *)(x)), (x)=NULL : (x)))
