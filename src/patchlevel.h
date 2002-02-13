657397d318b1a7a4af46e252f642799e38fd25b9
lwl <>
2002-02-13 01:53:53 +0000

    3.1a9

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -7,8 +7,8 @@
 #define __PATCHLEVEL_H
 
 #define	MUSH_VERSION		"3.1"		/* Base version number */
-#define PATCHLEVEL		8		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"09/27/2001"	/* Source release date */
+#define PATCHLEVEL		9		/* Patchlevel or beta number */
+#define	MUSH_RELEASE_DATE	"02/12/2002"	/* Source release date */
 #define MUSH_RELEASE_STATUS	2		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
