4d6b93801fe80f678b8ec4e6c4640d64fe9b13a3
lwl <>
2002-05-09 04:03:37 +0000

    3.0p4

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -4,7 +4,7 @@
 #include "copyright.h"
 
 #define	MUSH_VERSION		"3.0"		/* Base version number */
-#define PATCHLEVEL		3		/* Patch sequence number */
-#define	MUSH_RELEASE_DATE	"10/21/2001"	/* Source release date */
+#define PATCHLEVEL		4		/* Patch sequence number */
+#define	MUSH_RELEASE_DATE	"05/08/2002"	/* Source release date */
 
 /* #define BETA			1	*/	/* Define if a BETA release */
