5023ac0114227e487e5ea97961191ef3389a2280
lwl <>
2002-05-09 05:31:20 +0000

    3.1p1 beta release!

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -7,8 +7,8 @@
 #define __PATCHLEVEL_H
 
 #define	MUSH_VERSION		"3.1"		/* Base version number */
-#define PATCHLEVEL		10		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"02/13/2002"	/* Source release date */
-#define MUSH_RELEASE_STATUS	2		/* 0 gamma, 1 beta, 2 alpha */
+#define PATCHLEVEL		1		/* Patchlevel or beta number */
+#define	MUSH_RELEASE_DATE	"05/08/2002"	/* Source release date */
+#define MUSH_RELEASE_STATUS	1		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
