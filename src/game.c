f88860857fb8fd56ea35e91d12e1e72069c88d2a
lwl <>
2002-05-17 17:19:18 +0000

    Fixed typo: calls to CALL_ALL_MODULES_NOCACHE need function name in quotes.

--- src/game.c
+++ src/game.c
@@ -1823,7 +1823,7 @@ char *argv[];
 			/* Call all modules to write to GDBM */
 			
 			db_lock();
-			CALL_ALL_MODULES_NOCACHE(db_write, (void), ());
+			CALL_ALL_MODULES_NOCACHE("db_write", (void), ());
 			db_unlock();
 		} else {
 			db_write_flatfile(stdout, F_TINYMUSH, db_ver | db_flags);
