e0b9c6ac8f6d262443651ff9d657aa6e80c50763
lwl <>
2002-10-07 21:45:32 +0000

    Artistic License.

--- src/copyright.h
+++ src/copyright.h
@@ -18,8 +18,8 @@
  * Copyright (c) 1995, 1996, 1997, 1998, 1999, David Passmore.
  *
  * TinyMUSH 2.2 Source Code
- * Copyright (c) 1995, 1996, 1997, 1998, 1999, Lydia Leong, Jean Marie Diaz,
- * and Deborah Wilson-Hooker.
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, Lydia Leong,
+ * Jean Marie Diaz, and Deborah Wilson-Hooker.
  *
  * TinyMUSH 2.0 Source code
  * Copyright (c) 1991, Joseph Traub and Glenn Crocker. All rights reserved.
@@ -27,7 +27,6 @@
  * Based on TinyMUD code
  * Copyright (c) 1995, David Applegate, James Aspnes, Timothy Freeman,
  *                     and Bennet Yee. All rights reserved.
- *
  * (Modified by these authors from the original 1989, 1990 TinyMUD copyright.)
  *
  * ---
@@ -36,12 +35,12 @@
  * are derivations of TinyMUSH 2.0, which in turn is based upon TinyMUD.
  * There are also portions of source code derived from PennMUSH 1.50.
  *
- * This copyright notice is based upon the latest TinyMUD and TinyMUSH 2.0
- * copyright, with additional specific copyright information for TinyMUSH 3.
+ * The TinyMUD copyright follows below. PennMUSH, TinyMUSH 2.0, TinyMUSH 2.2,
+ * and TinyMUSH 3 follow the OSI-standard Artistic License.
  *
  * ---
  *
- * Revised TinyMUD / TinyMUSH 2.0 copyright:
+ * Revised TinyMUD copyright:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that: (1) source code distributions
@@ -58,26 +57,110 @@
  *
  * ---
  *
- * TinyMUSH 3 Copyright
+ * TinyMUSH 3 Copyright: The Artistic License
  *
- * Users of this software incur the obligation to make their best efforts
- * to inform the authors of noteworthy uses of this software.
+ * Preamble
  *
- * All materials developed as a consequence of the use of this software
- * shall duly acknowledge such use, in accordance with the usual standards
- * of acknowledging credit in academic research.
+ * The intent of this document is to state the conditions under which a
+ * Package may be copied, such that the Copyright Holder maintains some
+ * semblance of artistic control over the development of the package,
+ * while giving the users of the package the right to use and distribute
+ * the Package in a more-or-less customary fashion, plus the right to
+ * make reasonable modifications.
  *
- * TinyMUSH 3 may be used for commercial, for-profit applications,
- * subject to the following conditions: Commercial users must acknowledge
- * the origin of the software, retaining this copyright notice in some
- * prominent place.  Commercial users may charge only for the services
- * they provide, not for the TinyMUSH 3 software itself. Commercial users
- * incur the obligation to make their best efforts to inform the authors
- * of any commercial use of this software.
+ * Definitions:
+ *     - "Package" refers to the collection of files distributed by the
+ *       Copyright Holder, and derivatives of that collection of files
+ *       created through textual modification.
+ *     - "Standard Version" refers to such a Package if it has not been
+ *       modified, or has been modified in accordance with the wishes of
+ *       the Copyright Holder.
+ *     - "Copyright Holder" is whoever is named in the copyright or
+ *       copyrights for the package.
+ *     - "You" is you, if you're thinking about copying or distributing
+ *       this Package.
+ *     - "Reasonable copying fee" is whatever you can justify on the basis
+ *       of media cost, duplication charges, time of people involved, and
+ *       so on. (You will not be required to justify it to the Copyright
+ *       Holder, but only to the computing community at large as a market
+ *       that must bear the fee.)
+ *     - "Freely Available" means that no fee is charged for the item
+ *       itself, though there may be fees involved in handling the item. It
+ *       also means that recipients of the item may redistribute it under
+ *       the same conditions they received it.
  *
- * The authors have made no warranty or representation that the operation
- * of this software will be error-free. The authors are under no obligation
- * to provide any services, by way of maintenance, update, or otherwise.
+ * 1. You may make and give away verbatim copies of the source form of
+ * the Standard Version of this Package without restriction, provided
+ * that you duplicate all of the original copyright notices and
+ * associated disclaimers.
+ *
+ * 2. You may apply bug fixes, portability fixes and other modifications
+ * derived from the Public Domain or from the Copyright Holder. A Package
+ * modified in such a way shall still be considered the Standard Version.
+ *
+ * 3. You may otherwise modify your copy of this Package in any way,
+ * provided that you insert a prominent notice in each changed file
+ * stating how and when you changed that file, and provided that you do
+ * at least ONE of the following:
+ *
+ *   a) place your modifications in the Public Domain or otherwise make
+ *   them Freely Available, such as by posting said modifications to
+ *   Usenet or an equivalent medium, or placing the modifications on a
+ *   major archive site such as ftp.uu.net, or by allowing the Copyright
+ *   Holder to include your modifications in the Standard Version of the
+ *   Package.
+ *
+ *   b) use the modified Package only within your corporation or
+ *   organization.
+ *
+ *   c) rename any non-standard executables so the names do not conflict
+ *   with standard executables, which must also be provided, and provide
+ *   a separate manual page for each non-standard executable that
+ *   clearly documents how it differs from the Standard Version.
+ *   d) make other distribution arrangements with the Copyright Holder.
+ *
+ * 4. You may distribute the programs of this Package in object code or
+ * executable form, provided that you do at least ONE of the following:
+ *
+ *   a) distribute a Standard Version of the executables and library
+ *   files, together with instructions (in the manual page or
+ *   equivalent) on where to get the Standard Version.
+ *
+ *   b) accompany the distribution with the machine-readable source of
+ *   the Package with your modifications.
+ *
+ *   c) accompany any non-standard executables with their corresponding
+ *   Standard Version executables, giving the non-standard executables
+ *   non-standard names, and clearly documenting the differences in
+ *   manual pages (or equivalent), together with instructions on where
+ *   to get the Standard Version.
+ *
+ *   d) make other distribution arrangements with the Copyright Holder.
+ *
+ * 5. You may charge a reasonable copying fee for any distribution of
+ * this Package. You may charge any fee you choose for support of this
+ * Package. You may not charge a fee for this Package itself. However,
+ * you may distribute this Package in aggregate with other (possibly
+ * commercial) programs as part of a larger (possibly commercial)
+ * software distribution provided that you do not advertise this Package
+ * as a product of your own.
+ *
+ * 6. The scripts and library files supplied as input to or produced as
+ * output from the programs of this Package do not automatically fall
+ * under the copyright of this Package, but belong to whomever generated
+ * them, and may be sold commercially, and may be aggregated with this
+ * Package.
+ *
+ * 7. C or perl subroutines supplied by you and linked into this Package
+ * shall not be considered part of this Package.
+ *
+ * 8. The name of the Copyright Holder may not be used to endorse or
+ * promote products derived from this software without specific prior
+ * written permission.
+ *
+ * 9. THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
  * The authors may be reached via email at tinymush-dev@godlike.com.
  *
