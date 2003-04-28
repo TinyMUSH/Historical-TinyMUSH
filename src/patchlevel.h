a87997a9c201a24ff785edef4c06070475151616
lwl <>
2003-04-28 19:09:42 +0000

    Updated for 3.1b9 release.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -7,8 +7,8 @@
 #define __PATCHLEVEL_H
 
 #define	MUSH_VERSION		"3.1"		/* Base version number */
-#define PATCHLEVEL		0		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"02/12/2003"	/* Source release date */
-#define MUSH_RELEASE_STATUS	0		/* 0 gamma, 1 beta, 2 alpha */
+#define PATCHLEVEL		9		/* Patchlevel or beta number */
+#define	MUSH_RELEASE_DATE	"04/28/2003"	/* Source release date */
+#define MUSH_RELEASE_STATUS	1		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
