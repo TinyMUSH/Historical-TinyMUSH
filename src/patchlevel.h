953bbd553967888105b93227560393943f3fab4a
tyrspace <>
2006-06-05 19:17:21 +0000

    Release 3.1 Patchlevel 3

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -7,8 +7,8 @@
 #define __PATCHLEVEL_H
 
 #define	MUSH_VERSION		"3.1"		/* Base version number */
-#define PATCHLEVEL		2		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"11/11/2004"	/* Source release date */
+#define PATCHLEVEL		3		/* Patchlevel or beta number */
+#define	MUSH_RELEASE_DATE	"06/06/2006"	/* Source release date */
 #define MUSH_RELEASE_STATUS	0		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
