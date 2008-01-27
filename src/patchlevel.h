b6f686db6c66a012986583eb8d99c807a0ae7299
lwl <>
2008-01-27 18:12:04 +0000

    Prep for 3.1p5 release.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -8,7 +8,7 @@
 
 #define	MUSH_VERSION		"3.1"		/* Base version number */
 #define PATCHLEVEL		5		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"12/01/2007"	/* Source release date */
+#define	MUSH_RELEASE_DATE	"01/27/2008"	/* Source release date */
 #define MUSH_RELEASE_STATUS	0		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
