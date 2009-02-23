9125ca285d2e2e91588ebb66a6a8e9fb516c3819
lwl <>
2009-02-23 14:32:13 +0000

    objid %: substitution from PennMUSH

--- src/eval.c
+++ src/eval.c
@@ -392,6 +392,18 @@ dbref player;
  * We want '', ESC, ' ', '\', '[', '{', '(', '%', and '#'.
  */
 
+/* ASCII table for reference
+ *
+ * NUL,SOH,STX,ETX,EOT,ENQ,ACK,BEL,   BS,TAB,LF,VT,FF,CR,SO,SI,
+ * DLE,DC1,DC2,DC3,DC4,NAK,SYN,ETB,   CAN,EM,SUB,ESC,FS,GS,RS,US,
+ * space,!,",#,$,%,&,',               (,),*,+,comma,-,.,/,
+ * 0,1,2,3,4,5,6,7,                   8,9,:,;,<,=,>,?,
+ * @,A,B,C,D,E,F,G,                   H,I,J,K,L,M,N,O,
+ * P,Q,R,S,T,U,V,W,                   X,Y,Z,[,\,],^,_,
+ * `,a,b,c,d,e,f,g,                   h,i,j,k,l,m,n,o,
+ * p,q,r,s,t,u,v,w,                   x,y,z,{,|,},~,DEL
+ */
+
 char special_chartab[256] =
 {
     1,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,1,0,0,0,0,
@@ -921,6 +933,11 @@ char *cargs[];
 				break;
 			case '@':	/* Caller dbref */
 				safe_dbref(buff, bufc, caller);
+				break;
+ 		        case ':':	/* Enactor's objID */
+			        safe_dbref(buff, bufc, cause);
+				safe_chr(':', buff, bufc);
+				safe_ltos(buff, bufc, CreateTime(cause));
 				break;
 			case 'M':
 			case 'm':
