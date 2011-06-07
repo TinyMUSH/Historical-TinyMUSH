bebdb826713b8a97573fc77cd46dd99c592034a4
tyrspace <>
2011-06-07 00:57:18 +0000

    TinyMUSH 3.2 Gamma - 06/06/2011

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -7,8 +7,8 @@
 #define __PATCHLEVEL_H
 
 #define	MUSH_VERSION		"3.2"		/* Base version number */
-#define PATCHLEVEL		3		/* Patchlevel or beta number */
-#define	MUSH_RELEASE_DATE	"06/01/2010"	/* Source release date */
-#define MUSH_RELEASE_STATUS	1		/* 0 gamma, 1 beta, 2 alpha */
+#define PATCHLEVEL		0		/* Patchlevel or beta number */
+#define	MUSH_RELEASE_DATE	"06/06/2011"	/* Source release date */
+#define MUSH_RELEASE_STATUS	0		/* 0 gamma, 1 beta, 2 alpha */
 
 #endif /* __PATCHLEVEL_H */
